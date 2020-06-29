0023f009dfe54b14113f9ade8d120543b4f3a602
rodrigoferreira <rf121285>
2020-06-29 02:05:14 +0100

    more clean up

--- app/src/test/java/com/rf/dropchallenge/CheckBreweryProblemUseCaseTest.kt
+++ app/src/test/java/com/rf/dropchallenge/CheckBreweryProblemUseCaseTest.kt
@@ -1,59 +1,132 @@
 package com.rf.dropchallenge
 
-import com.rf.dropchallenge.domain.model.InputBeer
-import com.rf.dropchallenge.domain.model.InputCustomer
+import com.rf.dropchallenge.domain.repository.FileRepository
 import com.rf.dropchallenge.domain.usecase.CheckBreweryProblemUseCase
 import com.rf.dropchallenge.domain.usecase.GetCustomersFromInputFileUseCase
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import java.io.BufferedReader
 import java.io.IOException
-import java.lang.Exception
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @ExperimentalCoroutinesApi
 class CheckBreweryProblemUseCaseTest {
+
     private val checkBreweryProblemUseCase = CheckBreweryProblemUseCase()
 
+    @MockK
+    private lateinit var fileRepository: FileRepository
+
+    @MockK
+    private lateinit var getCustomersFromInputFileUseCase: GetCustomersFromInputFileUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        getCustomersFromInputFileUseCase = GetCustomersFromInputFileUseCase(fileRepository)
+    }
+
     @Test
-    fun `brewery problem success 1`() {
+    fun `brewery problem success 1`() = runBlockingTest {
 
         val inputStream = javaClass.getResourceAsStream("/test_input_1.txt") ?: throw IOException()
 
-        val content = inputStream.bufferedReader()
-            .use(BufferedReader::readText)
-
-        val beersAndCustomers = getBeersAndCustomersFromInputString(content)
-
-        try {
-            val result = checkBreweryProblemUseCase.checkBreweryProblem(
-                beersAndCustomers.numBeers,
-                beersAndCustomers.customers
-            )
-            val resultString = result.joinToString(" ", transform = { it.type })
-            Assert.assertEquals("C C C C B", resultString)
-        } catch (e: Exception) {
+        coEvery { fileRepository.getInputFile() }.coAnswers {
+            inputStream.bufferedReader()
+                .use(BufferedReader::readText)
         }
+
+        val beersAndCustomers = getCustomersFromInputFileUseCase.getInputFileBeerAndCustomers()
+
+        val result = checkBreweryProblemUseCase.checkBreweryProblem(
+            beersAndCustomers.numBeers,
+            beersAndCustomers.customers
+        )
+        val resultString = result.joinToString(" ", transform = { it.type })
+        Assert.assertEquals("C C C C B", resultString)
+    }
+
+    @Test
+    fun `brewery problem success 2`() = runBlockingTest {
+
+        val inputStream = javaClass.getResourceAsStream("/test_input_3.txt") ?: throw IOException()
+
+        coEvery { fileRepository.getInputFile() }.coAnswers {
+            inputStream.bufferedReader()
+                .use(BufferedReader::readText)
+        }
+
+        val beersAndCustomers = getCustomersFromInputFileUseCase.getInputFileBeerAndCustomers()
+
+        val result = checkBreweryProblemUseCase.checkBreweryProblem(
+            beersAndCustomers.numBeers,
+            beersAndCustomers.customers
+        )
+        val resultString = result.joinToString(" ", transform = { it.type })
+        Assert.assertEquals("C B C B C", resultString)
+
 
     }
 
     @Test
-    fun `brewery problem error 1`() {
+    fun `brewery problem success 3`() = runBlockingTest {
+
+        val inputStream = javaClass.getResourceAsStream("/test_input_4.txt") ?: throw IOException()
+
+        coEvery { fileRepository.getInputFile() }.coAnswers {
+            inputStream.bufferedReader()
+                .use(BufferedReader::readText)
+        }
+
+        val beersAndCustomers = getCustomersFromInputFileUseCase.getInputFileBeerAndCustomers()
+
+        val result = checkBreweryProblemUseCase.checkBreweryProblem(
+            beersAndCustomers.numBeers,
+            beersAndCustomers.customers
+        )
+        val resultString = result.joinToString(" ", transform = { it.type })
+        Assert.assertEquals("B B", resultString)
+    }
+
+    @Test
+    fun `brewery problem success 4`() = runBlockingTest {
+
+        val inputStream = javaClass.getResourceAsStream("/test_input_5.txt") ?: throw IOException()
+
+        coEvery { fileRepository.getInputFile() }.coAnswers {
+            inputStream.bufferedReader()
+                .use(BufferedReader::readText)
+        }
+
+        val beersAndCustomers = getCustomersFromInputFileUseCase.getInputFileBeerAndCustomers()
+
+        val result = checkBreweryProblemUseCase.checkBreweryProblem(
+            beersAndCustomers.numBeers,
+            beersAndCustomers.customers
+        )
+        val resultString = result.joinToString(" ", transform = { it.type })
+        Assert.assertEquals("B B B", resultString)
+    }
+
+    @Test
+    fun `brewery problem error 1`() = runBlockingTest {
 
         val inputStream = javaClass.getResourceAsStream("/test_input_2.txt") ?: throw IOException()
 
-        val content = inputStream.bufferedReader()
-            .use(BufferedReader::readText)
+        coEvery { fileRepository.getInputFile() }.coAnswers {
+            inputStream.bufferedReader()
+                .use(BufferedReader::readText)
+        }
 
-        val beersAndCustomers = getBeersAndCustomersFromInputString(content)
+        val beersAndCustomers = getCustomersFromInputFileUseCase.getInputFileBeerAndCustomers()
 
         try {
-            val result = checkBreweryProblemUseCase.checkBreweryProblem(
+            checkBreweryProblemUseCase.checkBreweryProblem(
                 beersAndCustomers.numBeers,
                 beersAndCustomers.customers
             )
@@ -61,27 +134,5 @@ class CheckBreweryProblemUseCaseTest {
             println(e)
             Assert.assertEquals("No solution exists", e.message)
         }
-    }
-
-    private fun getBeersAndCustomersFromInputString(inputString: String): GetCustomersFromInputFileUseCase.BeersAndCustomers {
-        val splitContent = inputString.split("\n")
-        val customers = mutableListOf<InputCustomer>()
-
-        val numBeers = inputString[0].toString().toInt()
-
-        for (i in 1 until splitContent.size) {
-            val noSpaceLine = splitContent[i].replace(" ", "")
-            val beers = mutableListOf<InputBeer>()
-            for (j in 0 until noSpaceLine.length - 1 step 2) {
-                beers.add(
-                    InputBeer(
-                        id = noSpaceLine[j].toString().toInt(),
-                        type = noSpaceLine[j + 1].toString()
-                    )
-                )
-            }
-            customers.add(InputCustomer(beers))
-        }
-        return GetCustomersFromInputFileUseCase.BeersAndCustomers(numBeers, customers)
     }
 }
